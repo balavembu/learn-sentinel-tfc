98ae3f9819e3f2963087ae83fbe75cd011c9ef98
balavembu <90377726+balavembu@users.noreply.github.com>
2021-09-24 00:31:02 -0700

    Reverting to the name of the Org Name
    This will not trigger as I changed the repo to learn-terraform-cost-estimation

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "idlyvada-try"
+    organization = "idlyvada"
 
     workspaces {
       name = "sentinel-example"
